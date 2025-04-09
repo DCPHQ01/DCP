09a81ff464e83181bd47c881a1f2dab651a407dc
daxvinci <ebukaokoro40@yahoo.com>
2025-04-09 14:02:29 +0100

    react template for dcp

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes } from "react-router"
+import { Route } from "react-router"
+import Home from "./pages/Home"
+import Career from "./pages/Career"
+import Contact from "./pages/Contact"
+import Service from "./pages/Service"
+import Product from "./pages/Product"
+import Project from "./pages/Project"
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="career" element={<Career />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="service" element={<Service />} />
+        <Route path="product" element={<Product />} />
+        <Route path="project" element={<Project />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
